850bd6ce09c85efd93f3937164bcecb70ccaa057
Murat BULUT <83401947+muratbulutofficial@users.noreply.github.com>
2021-04-30 00:44:49 +0300

    Updated Repository V.1.0

--- MvcBootcamp.WebUI/Controllers/CategoryController.cs
+++ MvcBootcamp.WebUI/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcBootcamp.BLL.Concrete;
+using MvcBootcamp.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +11,30 @@ namespace MvcBootcamp.WebUI.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
         public ActionResult Index()
         {
 
             return View();
+        }
+
+        public ActionResult GetList()
+        {
+            var categorylist = categoryManager.GetList();
+            return View(categorylist);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            categoryManager.Add(category);
+            return RedirectToAction("GetList");
         }
     }
 }
