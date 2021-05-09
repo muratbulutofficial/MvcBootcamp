8af164ce67864b37bb167818379400033555658b
Murat BULUT <83401947+muratbulutofficial@users.noreply.github.com>
2021-05-09 16:44:22 +0300

    Updated Repository V.1.6

---
+++ MvcBootcamp.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootcamp.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcBootcamp.WebUI/Controllers/PanelCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using MvcBootcamp.BLL.Abstract;
+using MvcBootcamp.BLL.DependencyResolvers.Ninject;
+using MvcBootcamp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootcamp.WebUI.Controllers
+{
+    public class PanelCategoryController : Controller
+    {
+        public PanelCategoryController()
+        {
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();
+        }
+        private ICategoryService _categoryService;
+        // GET: PanelCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult GetList()
+        {
+            return View(_categoryService.GetList());
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category category)
+        {
+            try
+            {
+                _categoryService.Add(category);
+                return RedirectToAction("GetList");
+            }
+            catch (Exception exception)
+            {
+                ModelState.AddModelError("Name", exception.Message);
+            }
+
+            return View();
+        }
+    }
+}
